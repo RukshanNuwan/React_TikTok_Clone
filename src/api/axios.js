6278af194c411b02f294e3996844ddd233dc20e0
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-02-01 17:14:29 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const base_url = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'https://tiktok-cln-mern-backend.herokuapp.com'
 });
 
 export default base_url;
